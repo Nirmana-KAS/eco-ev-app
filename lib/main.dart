2b8bb826eebc5b179ba47df7ca71e73424d784b8
Shehan Nirmana <135942979+Nirmana-KAS@users.noreply.github.com>
2025-05-20 08:56:54 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,19 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp()); // Use const
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key}); // Use super parameter shorthand
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Eco EV App',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Eco EV App'),
-        ),
-        body: Center(
-          child: Text('Hello, world!'),
-        ),
+        appBar: AppBar(title: Text('Eco EV App')),
+        body: Center(child: Text('Hello, world!')),
       ),
     );
   }
